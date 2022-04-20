40658625ff2676bc08c7e3360a52df1fcf4d2a71
Wei Zhao <zhaowei@microsoft.com>
2022-04-20 23:26:21 +0800

    <update

---
+++ lib/st/rest_api.ex
@@ -0,0 +1,5 @@
+defmodule ST.RestAPI do
+  def request_access_token do
+    IO.puts ""
+  end
+end
---
+++ lib/st/rg.ex
@@ -0,0 +1,14 @@
+# Module for some operation for resource group
+defmodule ST.RG do
+  def list_resource_groups_from(subscription_id) do
+    IO.puts "TODO"
+  end
+
+  def delete_resource_group(rg_id) do
+    IO.puts "TODO"
+  end
+
+  def exist?(rg_id) do
+    IO.puts "TODO"
+  end
+end
